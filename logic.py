c3fee82eb0e776fb72b71e86132063584609b341
SinPhourivath <163083945+SinPhourivath@users.noreply.github.com>
2024-07-02 01:28:45 +0700

    feat: complete minimization function
    - Complete sub method merge equivalent
    - Complete sub method reconstruct
    - Change from using own algorithm to find possible pair of state to using built in combinations function
    - Other optimization so some operation won't loop unnecessary

--- logic.py
+++ logic.py
@@ -1,8 +1,10 @@
+from itertools import combinations
+
 class FSM:
-    def __init__(self, states, start_state, end_states, symbols, transitions):
+    def __init__(self, states, start_state, final_states, symbols, transitions):
         self.states = states
         self.start_state = start_state
-        self.final_states = end_states
+        self.final_states = final_states
         self.symbols = symbols
         self.transitions = transitions
 
@@ -32,6 +34,7 @@ class FSM:
         else:
             print("String rejected")
 
+
     # Method for eliminating unreachable state
     def removeUnreachable(self):
         # Using Breadth First Search algorithm to find all reachable state
@@ -52,7 +55,7 @@ class FSM:
                 new_states.append(state)
         self.states = new_states
 
-        # Delete transitions to unreachable states
+        # Delete transitions from unreachable states
         new_transitions = {}
         for key in self.transitions:
             state, symbol = key
@@ -63,21 +66,77 @@ class FSM:
 
 
     # Method for finding equivalent state
-    def mergeEquivalent(self):
+    def findEquivalent(self):
         # Setup all possible combination of state
+        # Note: This is not transistion table. Key are made from two states and value is boolean
         distinguishable = {}
-        for p in self.states:
-            for q in self.states:
-                if p < q:
-                    distinguishable[(p, q)] = False
+        pairs = list(combinations(self.states, 2))
+        for pair in pairs:
+            distinguishable[pair] = False
 
         # Mark distinguishable if one is normal and one is final state
-        for p, q in distinguishable:
+        for key in distinguishable.keys():
+            p, q = key
             if (p in self.final_states) ^ (q in self.final_states):
                 distinguishable[(p, q)] = True
 
-        # Continue marking
+        # Continue marking untill no state 
+        iterate = True
+        while iterate:
+            iterate = False
+            for p, q in pairs:
+                if not distinguishable[(p, q)]:
+                    for symbol in self.symbols:
+                        p_next = self.transition(p, symbol)
+                        q_next = self.transition(q, symbol)
+                        if p_next != q_next and distinguishable[(p_next, q_next)]:
+                            distinguishable[(p, q)] = True
+                            iterate = True
+                            break
         
+        # Form new state
+        equivalent_classes = []
+        used_state = set()
+        # Iterate over each state to form new state
+        for p in self.states:
+            if p not in used_state:
+                equivalent_class = [p]
+                used_state.add(p)
+                available_state = list(set(self.states) - used_state)
+                for q in available_state:
+                    # Check if state q is equivalent to state p
+                    if distinguishable[(p, q)] == True:
+                        equivalent_class.append(q)
+                        used_state.add(q)
+                equivalent_classes.append(equivalent_class)
+        
+        return equivalent_classes
+    
+    
+    def reconstruct(self, eq_classes):
+        new_states = ['q' + str(count) for count in range(len(eq_classes))]
+        new_start_state = None
+        new_final_states = set()
+        new_transitions = {}
+
+        state_mapping = {}
+        for i, eq_class in enumerate(eq_classes):
+            for state in eq_class:
+                state_mapping[state] = new_states[i]  # Map old state to new state
+                if state in self.final_states:
+                    new_final_states.add(new_states[i])  # If any state in the class is final, mark new state as final
+                if new_start_state is None and state in self.start_state:
+                    new_start_state = new_states[i]
+
+        # Create new transition function
+        for i, eq_class in enumerate(eq_classes):
+            for state in eq_class:
+                for symbol in self.symbols:
+                    next_state = self.transitions[(state, symbol)]  # Get the transition of the old state
+                    new_transitions[(new_states[i], symbol)] = state_mapping[next_state]  # Map to the new transition
+               
+        # Return the new FSM
+        return FSM(new_states, new_start_state, new_final_states, self.symbols, new_transitions)
 
     # Method for minimizing if DFA
     def minimize(self):
@@ -86,35 +145,35 @@ class FSM:
 
         self.removeUnreachable()
 
-        self.mergeEquivalent()
+        equivalent_states = self.findEquivalent()
+
+        minimized_fsm = self.reconstruct(equivalent_states)
+
+        return minimized_fsm
 
 
-
-fsm_states = ['q0', 'q1', 'q2', 'q3', 'q4', 'q5']
+fsm_states = ['q0', 'q1', 'q2', 'q3', 'q4']
 fsm_start_state = 'q0'
-fsm_final_states = ['q1', 'q2']
+fsm_final_states = ['q1', 'q3']
 fsm_symbols = ['a', 'b']
 fsm_transitions = {
     ('q0', 'a'): 'q1',
-    ('q0', 'b'): 'q4',
-    ('q1', 'a'): 'q4',
+    ('q0', 'b'): 'q1',
+    ('q1', 'a'): 'q2',
     ('q1', 'b'): 'q2',
-    ('q2', 'a'): 'q2',
-    ('q2', 'b'): 'q2',
-    ('q3', 'a'): 'q0',
-    ('q3', 'b'): 'q4',
-    ('q4', 'a'): 'q2',
-    ('q4', 'b'): 'q1',
-    ('q5', 'a'): 'q1',
-    ('q5', 'b'): 'q2'
+    ('q2', 'a'): 'q3',
+    ('q2', 'b'): 'q3',
+    ('q3', 'a'): 'q2',
+    ('q3', 'b'): 'q2',
+    ('q4', 'a'): 'q3',
+    ('q4', 'b'): 'q2'
 }
 
 fsm = FSM(fsm_states, fsm_start_state, fsm_final_states, fsm_symbols, fsm_transitions)
 
 print(fsm)
+print()
 
-fsm.isAccept("bba")
+new_fsm = fsm.minimize()
 
-fsm.minimize()
-
-print(fsm)
+print(new_fsm)
